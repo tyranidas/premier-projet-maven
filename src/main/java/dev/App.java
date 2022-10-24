50f18daaf137e22784cb988fccd483a81a74dc21
Manuel <tyranidas@hotmail.com>
2022-10-24 16:08:29 +0200

    TP3 profile

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,16 +1,22 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	String asciiArt = FigletFont.convertOneLine("App");
-        System.out.println(asciiArt);
-    }
+public class App {
+	public static void main(String[] args) {
+  String titre = ResourceBundle.getBundle("application").getString("titre"); 
+  String asciiArt = FigletFont.convertOneLine(titre);
+  System.out.println(asciiArt);
+  
+  //affichage de l'environnement
+  String environnement = ResourceBundle.getBundle("application").getString("environnement");
+  System.out.println("Environnement = "+environnement);
+  
+  
+  }
 }
